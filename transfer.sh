607ba2980899e1f23be06cc6263130785e4ce896
AmirDapl <Amir@daplcorp.com>
2024-11-18 14:49:58 -0600

    Updated transfer script
    Changed Line to fix double header issue and changed h3 to h2

--- transfer.sh
+++ transfer.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# Set variables for paths
+# This script copies the builtins documentation from the kadena-io/pact-5 repository
+# Run with `bash transfer.sh`
+
+# Path Variables
 REPO_URL="https://github.com/kadena-io/pact-5.git"
 TEMP_DIR="temp-pact-5"
 DOCS_DIR="docs/pact-5"
@@ -23,8 +26,17 @@ mkdir -p $DOCS_DIR
 echo "Copying builtins files into $DOCS_DIR..."
 cp -r $TEMP_DIR/$BUILTINS_DIR/* $DOCS_DIR/
 
-# Clean up by removing the temporary directory
+# Find all markdown files and change lines that start with ### to ## if not the first line
+echo "Updating markdown files..."
+find $DOCS_DIR -name "*.md" | while read -r file; do
+    # change line 1 from ## to #
+    sed -i '' '1s/^##/#/' "$file"
+    # change lines 2 to end from ### to ##
+    sed -i '' '2,$s/^###/##/' "$file"
+done
+
+# Clean up 
 echo "Cleaning up..."
 rm -rf $TEMP_DIR
 
-echo "Done! The builtins files have been merged into $DOCS_DIR."
+echo "Done! The builtins files have been merged into $DOCS_DIR, and all headings have been updated."
