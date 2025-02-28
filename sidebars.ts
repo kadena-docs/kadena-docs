1d28c930fee112d1ef720ab740fb51940de3d031
Lisa Gunn <lisa@kadena.io>
2025-02-28 15:55:10 -0800

    Add first iteration of estimating gas to the coding projects

--- sidebars.ts
+++ sidebars.ts
@@ -489,6 +489,7 @@ const sidebars: SidebarsConfig = {
                     'coding-projects/gas-station',
                     'coding-projects/loans',
                     'coding-projects/local-testing',
+                    'coding-projects/estimate-gas',
                     'coding-projects/pact-server-api',
                ],
           },
